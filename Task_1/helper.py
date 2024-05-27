b06216c8dad1c965168b4236bcfc4929b14415cd
MehrShahbaz <mehrshahbaz7@gmail.com>
2024-05-27 11:59:50 +0200

    Added Comments

--- Task_1/helper.py
+++ Task_1/helper.py
@@ -1,66 +1,79 @@
 import requests
 import json
-import re
 from bs4 import BeautifulSoup
 
+# Constant to use for printing separation lines
 breakPrint = "\n==========================================================================================\n"
+# HTTP status code for a successful response
 response_ok = 200
 
-
 def custom_print(data, endPrint=False):
+    """
+    Print data with a custom separator before and after the content.
+    If endPrint is True, print the separator after the content as well.
+    """
     print(breakPrint)
     print(data)
     if endPrint:
         print(breakPrint)
 
-
 def print_list(data):
+    """
+    Print each item in the list using custom_print function.
+    """
     for x in data:
         custom_print(x)
     print(breakPrint)
 
-
 def write_in_file(data, file_name):
+    """
+    Write data to a file in JSON format. If an error occurs, print an error message.
+    """
     try:
         f = open(file_name, "w")
         json.dump(data, f)
         f.close()
     except:
-        print(f"Error Writting to the file: {file_name}")
-
+        print(f"Error Writing to the file: {file_name}")
 
 def find_next_page_url(soup, base_url):
+    """
+    Find the URL for the next page from the BeautifulSoup object.
+    If a next page link exists, return the full URL; otherwise, return None.
+    """
     next_li = soup.select_one(".next a")
-
     return base_url + next_li.get("href") if next_li else None
 
-
-def remove_character(string, char):
-    result = re.sub(char, "", string)
-
-    return result
-
+def remove_character(string, chars):
+    """
+    Remove specific characters from a string.
+    """
+    return "".join([c for c in string if c not in chars])
 
 def format_quote(data):
-    data = remove_character(data, "”")
-    data = remove_character(data, "“")
-
-    return data
-
+    """
+    Format the quote by removing specific characters (e.g., quotation marks).
+    """
+    return remove_character(data, ["”", "“"])
 
 def fetch_page(url):
+    """
+    Fetch the HTML content of a given URL using requests and parse it with BeautifulSoup.
+    If an error occurs, print an error message and return None.
+    """
     try:
         response = requests.get(url)
         if response.status_code != response_ok:
             raise Exception("Bad Response")
-
         return BeautifulSoup(response.text, "html.parser")
     except:
         print(f"Error fetching the URL: {url}")
         return None
 
-
 def extract_quotes(soup, url):
+    """
+    Extract quotes, authors, and tags from the BeautifulSoup object and return them as a list of dictionaries.
+    """
     return [
         {
             "quote": format_quote(div.select_one("span.text").text),
@@ -70,3 +83,22 @@ def extract_quotes(soup, url):
         }
         for div in soup.select(".quote")
     ]
+
+def print_execution_time(start, end):
+    """
+    Print the time taken for an operation.
+    """
+    custom_print(f"Time taken = {end-start:.3f} Seconds", True)
+
+def convert_to_mbs(data):
+    """
+    Convert bytes to megabytes.
+    """
+    return data / (1024 * 1024)
+
+def print_memory_use(current, peak):
+    """
+    Print the current and peak memory usage in megabytes.
+    """
+    custom_print(f"Current memory usage: {convert_to_mbs(current):.2f} MB")
+    custom_print(f"Peak memory usage: {convert_to_mbs(peak):.2f} MB", True)
--- Task_1/main.py
+++ Task_1/main.py
@@ -1,26 +1,54 @@
+from timeit import default_timer as timer
+import tracemalloc
 from helper import (
     fetch_page,
     find_next_page_url,
     custom_print,
     write_in_file,
     extract_quotes,
+    print_execution_time,
+    print_memory_use,
+    convert_to_mbs,
 )
 
-
+# Method to get all the quotes from the URL
+# Returns a list of quotes
 def scrape_quotes(base_url):
     current_url = base_url
     data = []
     while current_url:
+        # Fetch the page and parse it with BeautifulSoup
         soup = fetch_page(current_url)
         if soup:
+            # Extract quotes from the page and add them to the data list
             data.extend(extract_quotes(soup, current_url))
             custom_print("Current Url: " + current_url, True)
+            # Find the URL for the next page
             current_url = find_next_page_url(soup, base_url)
         else:
             break
     return data
 
+# Start the timer and memory tracer
+time_start = timer()
+tracemalloc.start()
 
+# Base URL for the quotes
 base_url = "https://quotes.toscrape.com"
+
+# Get all the quotes from the website
 quotes = scrape_quotes(base_url)
+
+# Write the quotes to a file named "data.json"
 write_in_file(quotes, "data.json")
+
+# Get the current and peak memory usage
+current, peak = tracemalloc.get_traced_memory()
+
+# Stop the memory tracer and end the timer
+tracemalloc.stop()
+time_end = timer()
+
+# Print the execution time and memory usage in MB
+print_execution_time(time_start, time_end)
+print_memory_use(convert_to_mbs(current), convert_to_mbs(peak))
